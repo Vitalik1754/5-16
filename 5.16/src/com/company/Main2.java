8158f83495b273bc0e9b7231f34165fa7974ce24
Vitalik1754 <99317811+Vitalik1754@users.noreply.github.com>
2022-05-04 15:36:45 +0300

    Add files via upload

---
+++ 5.16/src/com/company/Main2.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main2 {
+    public static void main(String[] args){
+    int q = 3;
+    int w = 4;
+    double e = Math.sqrt((q*q)+(w*w));
+    int r = (q*w)/2;
+    double t = q+w+e;
+    System.out.println("Площа прямокутного трикутника:"+r);
+    System.out.println("Периметер прямокутного трикутника"+t);
+    }
+}
---
+++ 5.16/src/com/company/Main3.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main3 {
+    public static void main(String[] args){
+        double q = Math.random()*102;
+        System.out.println("Згенероване число: "+q);
+        System.out.println("Кількість цифр: " + (String.valueOf(q).length()-1));
+    }
+}
